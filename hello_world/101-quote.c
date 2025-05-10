a17f38a3a4d18bad76f12bae3e19ec07f815ae63
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2025-05-10 03:53:18 -0700

    101-quote.c

---
+++ hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Failure)
+ */
+int main(void)
+{
+const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, message, 59);
+return (1);
+}
