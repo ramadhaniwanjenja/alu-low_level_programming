63f31cca7c9a47e33af96427289d9f7761152a68
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2025-05-18 03:26:01 -0700

    correct output

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-int main()
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-putchar('a');
+putchar('abcdefghijklmnopqrstuvwxyz');
 putchar('\n');
 return (0);
 }
