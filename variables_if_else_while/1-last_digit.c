3b9f70e7f9ad2f5abc0e67d983024f4f62c9d472
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2025-05-18 02:58:15 -0700

    betty coding style

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,10 +16,10 @@ n = rand() - RAND_MAX / 2;
 last_digit = n % 10;
 printf("Last digit of %d is %d and is ", n, last_digit);
 if (last_digit > 5)
-    printf("greater than 5\n");
+printf("greater than 5\n");
 else if (last_digit == 0)
-    printf("0\n");
+printf("0\n");
 else
-    printf("less than 6 and not 0\n");
+printf("less than 6 and not 0\n");
 return (0);
 }
