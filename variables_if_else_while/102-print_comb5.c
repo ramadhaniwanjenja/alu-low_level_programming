40d408ad7722d9cdd684e1a62d9f658490addf26
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2025-05-18 05:12:23 -0700

    102-print_comb5

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 0;
+while (i <= 98)
+{
+int j = i + 1;
+while (j <= 99)
+{
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+putchar(' ');
+putchar((j / 10) + '0');
+putchar((j % 10) + '0');
+if (i != 98 || j != 99)
+{
+putchar(',');
+putchar(' ');
+}
+j++;
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
