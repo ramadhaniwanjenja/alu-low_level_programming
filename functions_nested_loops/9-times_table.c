77471568b2b356376e17a7d14540e91d31eba01d
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2025-05-29 07:22:24 -0700

    9-times_table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * times_table - Prints the 9 times table, starting with 0
+ */
+void times_table(void)
+{
+int i = 0;
+while (i <= 9)
+{
+int j = 0;
+while (j <= 9)
+{
+int product = i * j;
+if (j > 0)
+{
+if (product < 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+if (product >= 10)
+{
+_putchar('0' + (product / 10));
+}
+else if (j > 0)
+{
+_putchar(' ');
+}
+_putchar('0' + (product % 10));
+j++;
+}
+_putchar('\n');
+i++;
+}
+}
