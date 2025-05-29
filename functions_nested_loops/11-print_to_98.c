94eaa6d097fc2b2fbb6b5fd434b598447405a290
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2025-05-29 07:40:39 -0700

    betty coding style

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_to_98 - Prints all natural numbers from n to 98, followed by a new line
+ * print_to_98 - Prints all natural numbers from n to 98,
+ * followed by a new line
  * @n: The starting number
  */
 void print_to_98(int n)
